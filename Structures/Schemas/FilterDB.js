607a1b4a586c386487d084f05e8aef952f8f9168
SkullGaming31 <canadianfan11@hotmail.com>
2023-03-03 06:17:54 -0330

    Delete Structures/Schemas directory

--- Structures/Schemas/FilterDB.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-module.exports = model('Filter', new Schema({
-  Guild: String,
-  Log: String,
-  Words: [String]
-})
-);
--- Structures/Schemas/WarningDB.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-module.exports = model("WarningDB", new Schema({
-    GuildID: String,
-    UserID: String,
-    UserTag: String,
-    Content: Array
-}))
