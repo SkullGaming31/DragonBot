cb611899eea1ff902a3251c236f95b757752254d
skullgaming31 <canadianfan11@hotmail.com>
2024-03-01 07:35:57 -0330

    fixed presenceUpdate to only log if a custom status is set

--- src/Events/Logs/presenceUpdate.ts
+++ src/Events/Logs/presenceUpdate.ts
@@ -6,8 +6,8 @@ import { Event } from '../../Structures/Event';
 export default new Event<'presenceUpdate'>('presenceUpdate', async (oldPresence: Presence | null, newPresence: Presence) => {
 	try {
 		const data = await ChanLogger.findOne({ Guild: newPresence.guild?.id }).catch((err: MongooseError) => { console.error(err.message); });
-		// console.log('oldPresence: ', oldPresence);
-		// console.log('newPresence: ', newPresence);
+		console.log('oldPresence: ', oldPresence);
+		console.log('newPresence: ', newPresence);
 
 		if (!data || data.enableLogs === false) return;
 
@@ -19,22 +19,24 @@ export default new Event<'presenceUpdate'>('presenceUpdate', async (oldPresence:
 		const Embed = new EmbedBuilder().setTitle(`${newPresence.guild?.name}[empty]`).setTimestamp();
 
 		// Check if oldPresence is not null and if it has activities
-		if (oldPresence && oldPresence.activities && oldPresence.activities.length > 0) {
+		if (oldPresence && oldPresence.activities && oldPresence.activities.length > 0 && oldPresence.activities[0].type === 4) {
 			// Access the old state only if oldPresence has activities
 			const oldState = oldPresence.activities[0]?.state;
 			const newState = newPresence.activities[0]?.state;
+			const oldStatus = oldPresence.status;
+			const newStatus = newPresence.status;
 
 			// Check if newState is different from oldState
 			if (newState && newState !== oldState) {
 				if (logsChannelOBJ?.type === ChannelType.GuildText) {
 					return logsChannelOBJ.send({
 						embeds: [
-							Embed.setDescription(`${newPresence.user?.globalName} status has changed from \`${oldState}\` to: \`${newState}\` \nStatus: ${newPresence.status}`)
+							Embed.setDescription(`${newPresence.user?.globalName} status has changed from \`${oldState}\` to: \`${newState}\` \noldStatus: ${oldStatus} to: newStatus: ${newStatus}`)
 						]
 					});
 				}
 			}
-		} else if (newPresence.activities && newPresence.activities.length > 0) {
+		} else if (newPresence.activities && newPresence.activities.length > 0 && newPresence.activities[0].type === 4) {
 			// Handle the case where oldPresence has no activities but newPresence does
 			const newState = newPresence.activities[0]?.state;
 
