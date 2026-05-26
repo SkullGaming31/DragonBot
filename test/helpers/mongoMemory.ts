b0256d09d0db8b2e96d1a9261104bc9e6eb1847a
skullgaminghq <skullgamingg31@gmail.com>
2026-05-26 14:00:34 -0230

    CI: use mongo service in GitHub Actions and prefer MONGO_URL in tests

--- test/helpers/mongoMemory.ts
+++ test/helpers/mongoMemory.ts
@@ -4,6 +4,14 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 let mongod: MongoMemoryServer | null = null;
 
 export async function startInMemoryMongo() {
+  // If a CI or external MongoDB URL is provided, connect to it instead of
+  // attempting to download a binary (prevents CI download failures).
+  const external = process.env.MONGO_URL ?? process.env.MONGODB_URI ?? process.env.CI_MONGO_URL;
+  if (external) {
+    await mongoose.connect(external);
+    return external;
+  }
+
   mongod = await MongoMemoryServer.create({ instance: { storageEngine: 'wiredTiger' } });
   const uri = mongod.getUri();
   await mongoose.connect(uri);
@@ -12,5 +20,6 @@ export async function startInMemoryMongo() {
 
 export async function stopInMemoryMongo() {
   await mongoose.disconnect();
+  // Only stop the in-memory server if we started it here
   if (mongod) await mongod.stop();
 }
